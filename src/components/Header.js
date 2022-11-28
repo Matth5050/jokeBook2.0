f2ddee33664685cda003b2ed74b534df7110dc95
Matth5050 <matthew.f.herbert@gmail.com>
2022-11-27 22:28:06 -0800

    updated header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect} from "react";
 import PropTypes from 'prop-types';
-import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
+import { Link} from "react-router-dom";
 import { auth } from "../firebase";
 
 export default function Header() {
 
-  
+  const usernameButton = {
+    border: 'none'
+  }
   
   // function decider() {
   //   if (isAuthenticated == false) {
@@ -18,21 +21,27 @@ export default function Header() {
 // useEffect(() => {
 //   decider();
 // }, [firebase.auth().currentUser] )
- 
- 
+function doSignOut() {
+  signOut(auth)
+    .then(function() {
+     
+    }).catch(function(error) {
+      
+    });
+}
 
   if (auth.currentUser == null) {
     return (
       <React.Fragment>
-        <nav class="navbar navbar-expand-lg bg-light">
+        <nav class="navbar navbar-expand-lg header">
           <div class="container-fluid">
             <a class="navbar-brand" href="#">JokeBook</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
             <div class="navbar-nav">
-              <a class="nav-link active" aria-current="page" href="#">Home</a>
-              <a class="nav-link" href="#">Hello</a>
+              <Link className="header" to="/signup"><button className="nav-item btn btn-outline-primary mx-2">Signup</button></Link>
+              <Link className="header" to="/login"><button className="nav-item btn btn-outline-primary">Login</button></Link>
             </div>
           </div>
         </nav>
@@ -41,25 +50,19 @@ export default function Header() {
   } else {
     return (
     <React.Fragment>
-        <nav class="navbar navbar-expand-lg bg-light">
+        <nav class="navbar navbar-expand-lg">
           <div class="container-fluid">
             <a class="navbar-brand" href="#">JokeBook</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
             <div class="navbar-nav">
-              <a class="nav-link active" aria-current="page" href="#">logged</a>
-              <a class="nav-link" href="#">test</a>
+            <button class="nav-item btn btn-outline-primary border border-0"><strong>test</strong></button>
+            <button type="click" onClick={() => doSignOut()} className="btn btn-outline-primary mx-2 border border-0">sign out</button>
             </div>
           </div>
         </nav>
       </React.Fragment>
     );
   }
- 
 }
-
-// Header.propTypes = {
-//   ticket: PropTypes.object,
-//   onEditTicket: PropTypes.func
-// };

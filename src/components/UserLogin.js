4406fa9d915178a57780cccbcf3b68bb20687b4b
Matth5050 <matthew.f.herbert@gmail.com>
2022-11-27 22:28:40 -0800

    cleaned up code and added navigating after login success

--- src/components/UserLogin.js
+++ src/components/UserLogin.js
@@ -1,9 +1,8 @@
 import React, { useState } from "react";
 import { auth } from "../firebase.js";
 import { signOut } from "firebase/auth";
-import { Link } from "react-router-dom";
 import { getAuth, setPersistence, signInWithEmailAndPassword, browserSessionPersistence, createUserWithEmailAndPassword } from "firebase/auth";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link, Navigate, useNavigate } from "react-router-dom";
 
 export default function UserSignUpIn() {  
 
@@ -15,18 +14,10 @@ export default function UserSignUpIn() {
   const [signUpSuccess, setSignUpSuccess] = useState(null);
   const [signInSuccess, setSignInSuccess] = useState(null);
   const [signOutSuccess, setSignOutSuccess] = useState(null);
+  const navigate = useNavigate();
 
-  function doSignUp(event) {
-    event.preventDefault();
-    const email = event.target.email.value;
-    const password = event.target.password.value;
-    createUserWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        setSignUpSuccess(`You've successfully signed up, ${userCredential.user.email}!`)
-      })
-      .catch((error) => {
-        setSignUpSuccess(`There was an error signing up: ${error.message}!`)
-      });
+  function navToHome() {
+    navigate('/');
   }
 
   function doSignIn(event) {
@@ -40,6 +31,7 @@ export default function UserSignUpIn() {
         return signInWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
         setSignInSuccess(`You've successfully signed in as ${userCredential.user.email}!`)
+        navToHome();
     })
     .catch((error) => {
       setSignInSuccess(`There was an error signing in: ${error.message}!`);
@@ -47,41 +39,8 @@ export default function UserSignUpIn() {
   })
   }
 
-  function doSignOut() {
-    signOut(auth)
-      .then(function() {
-        setSignOutSuccess("You have successfully signed out!");
-      }).catch(function(error) {
-        setSignOutSuccess(`There was an error signing out: ${error.message}!`);
-      });
-  }
-
-
-
-  // return (
-  //   <React.Fragment>
-  //     <h1>Sign up</h1>
-  //     {signUpSuccess}
-  //     <form onSubmit={doSignUp}>
-  //       <input
-  //         type='text'
-  //         name='email'
-  //         placeholder='email' />
-  //       <input
-  //         type='password'
-  //         name='password'
-  //         placeholder='Password' />
-  //       <button type='submit'>Sign up</button>
-  //     </form>
-  //   </React.Fragment>
-  // );
-
-
   return (
     <React.Fragment>
-      ...
-
-      <h1>Sign In</h1>
       <div>
         <div className="card text-center mx-auto p-3 login-card" style={cardWidth}>
           <div>
@@ -104,13 +63,13 @@ export default function UserSignUpIn() {
                 className="form-control" 
                 id="exampleInputPassword1" 
                 placeholder="password"></input>
-                <p className="small">forgot password?</p>
+                
               </div>
               <button type="submit" className="sports btn">Login</button>
             </form>
           </div>
         </div>
-        <p className="text-center mt-3 color-style">New to LeaderBet? <Link to="/register">Register an account here!</Link></p>
+        <p className="text-center mt-3 color-style">New to HumorHub? <Link to="/register">Register an account here!</Link></p>
       </div>
     </React.Fragment>
   );

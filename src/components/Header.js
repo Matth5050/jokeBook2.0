dafa2c1c60407273689a8f594f226c51868a1319
Matth5050 <matthew.f.herbert@gmail.com>
2022-12-05 20:50:07 -0800

    udpated header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,30 +1,26 @@
 import React, { useState, useEffect} from "react";
 import PropTypes from 'prop-types';
 import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
-import { Link} from "react-router-dom";
+import { Link, useNavigate} from "react-router-dom";
 import { auth } from "../firebase";
+
 
 export default function Header() {
 
   const usernameButton = {
-    border: 'none'
+    borderColor: 'white'
   }
   
-  // function decider() {
-  //   if (isAuthenticated == false) {
-  //     setTest('not logged');
-  //   } else {
-  //     setTest('logged');
-  //   }
-  // }
+  const brand = {
+    textDecoration: "none"
+  }
 
-// useEffect(() => {
-//   decider();
-// }, [firebase.auth().currentUser] )
+const navigate = useNavigate();
+
 function doSignOut() {
   signOut(auth)
     .then(function() {
-     
+      navigate('/login');
     }).catch(function(error) {
       
     });
@@ -35,11 +31,11 @@ function doSignOut() {
       <React.Fragment>
         <nav class="navbar navbar-expand-lg header">
           <div class="container-fluid">
-            <p className="title navbar-brand">JokeBook</p>
-            <div class="navbar-nav">
-              <a href="https://chrome.google.com/webstore/detail/jokebook/nlhdhpfhfopniaajbgfichnlpbnndhfk"><button className=" border border-0 nav-item btn btn-outline-primary">Get the Chrome extension here!</button></a>
-              <Link className="header" to="/signup"><button className="nav-item btn btn-outline-primary mx-2">Signup</button></Link>
-              <Link className="header" to="/login"><button className="nav-item btn btn-outline-primary">Login</button></Link>
+            <p className="navbar-brand title"><Link to="/" className="title" style={brand}>JokeBook</Link></p>
+            <div class="navbar-nav bodyText">
+              <a href="https://chrome.google.com/webstore/detail/jokebook/nlhdhpfhfopniaajbgfichnlpbnndhfk"><button className="border border-0 nav-item btn btn-outline-primary bodyText">Get the Chrome extension here!</button></a>
+              <Link className="header" to="/login"><button className="nav-item btn btn-outline-primary bodyText" style={usernameButton}>Login</button></Link>
+              <Link className="header" to="/signup"><button className="nav-item btn btn-outline-primary mx-2 bodyText" style={usernameButton}>Signup</button></Link>
             </div>
           </div>
         </nav>
@@ -52,9 +48,9 @@ function doSignOut() {
           <div class="container-fluid">
             <p className="title navbar-brand">JokeBook</p>
             <div class="navbar-nav mb-3">
-              <a href="https://chrome.google.com/webstore/detail/jokebook/nlhdhpfhfopniaajbgfichnlpbnndhfk"><button className=" border border-0 nav-item btn btn-outline-primary">Get the Chrome extension here!</button></a>
-              <button class="nav-item btn btn-outline-primary border border-0"><strong>{auth.currentUser.displayName}</strong></button>
-              <button type="click" onClick={() => doSignOut()} className="btn btn-outline-primary mx-2 border border-0">sign out</button>
+              <a href="https://chrome.google.com/webstore/detail/jokebook/nlhdhpfhfopniaajbgfichnlpbnndhfk"><button className=" border border-0 nav-item btn btn-outline-primary bodyText">Get the Chrome extension here!</button></a>
+              <button class="nav-item btn btn-outline-primary border-white bodyText"><strong>{auth.currentUser.displayName}</strong></button>
+              <button type="click" onClick={() => doSignOut()} className="btn btn-outline-primary mx-2 border-white bodyText">sign out</button>
             </div>
           </div>
         </nav>

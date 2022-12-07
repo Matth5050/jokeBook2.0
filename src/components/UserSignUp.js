ce0526817bf699d1413a70657c687944009ac811
Matth5050 <matthew.f.herbert@gmail.com>
2022-12-06 21:08:01 -0800

    made font size larger

--- src/components/UserSignUp.js
+++ src/components/UserSignUp.js
@@ -55,7 +55,7 @@ export default function UserSignUpIn() {
       <LogHeader />
         <div className="card text-center mx-auto p-3 bg-transparent border-0" style={cardWidth}>
           <div>
-            <h3 className="mb-3 card-title title">Sign-Up</h3>
+            <h3 className="mb-3 card-title headings fs-1">Sign-Up</h3>
             <form className="form" onSubmit={doSignUp}>
                 <div className="mb-3">
                   <input 
@@ -106,44 +106,4 @@ export default function UserSignUpIn() {
         <p className="text-center mt-3 color-style smallText">Already have an account? <Link className="linkText" to="/login">Login here</Link></p>
     </React.Fragment>
   );
-
-
-
-  // return (
-  //   <React.Fragment>
-  //     ...
-
-  //     <h1>Sign In</h1>
-  //     <div>
-  //       <div className="card text-center mx-auto p-3 login-card" style={cardWidth}>
-  //         <div>
-  //           <h3 className="mb-3 card-title">Login</h3>
-  //           <form className="form" onSubmit={doSignIn}>
-  //             <div className="mb-3">
-  //               <input 
-  //               type="email" 
-  //               className="form-control col-xs-3"
-  //               name="signinEmail" 
-  //               id="exampleInputEmail1" 
-  //               size="50" 
-  //               aria-describedby="emailHelp" 
-  //               placeholder="email"></input>
-  //             </div>
-  //             <div className="mb-3">
-  //               <input 
-  //               type="password" 
-  //               name="signinPassword"
-  //               className="form-control" 
-  //               id="exampleInputPassword1" 
-  //               placeholder="password"></input>
-  //               <p className="small">forgot password?</p>
-  //             </div>
-  //             <button type="submit" className="sports btn">Login</button>
-  //           </form>
-  //         </div>
-  //       </div>
-  //       <p className="text-center mt-3 color-style">New to LeaderBet? <Link to="/register">Register an account here!</Link></p>
-  //     </div>
-  //   </React.Fragment>
-  // );
 }

bb2851f79a5dc3b459177dfc43ddba14c5d982f6
Matth5050 <matthew.f.herbert@gmail.com>
2022-12-10 22:00:29 -0800

    added onAuthChange to properly get userEmail

--- src/components/UserJokes.js
+++ src/components/UserJokes.js
@@ -1,12 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import { collection, onSnapshot, doc, updateDoc, deleteDoc, getDoc } from 'firebase/firestore';
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 import  { auth, db } from './../firebase.js';
 import SubmittedJokes from './SubmittedJokes.js';
 
 function UserJokes() {
 
-  const [userJokesList, setUserJokesList] = useState();
+  const [userJokesList, setUserJokesList] = useState(null);
   const [jokesLoaded, setJokesLoaded] = useState(false);
+  const [karmaList, setKarmaList] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const grabObject = window.sessionStorage.getItem(sessionStorage.key(auth.currentUser));
+  const parseObject = JSON.parse(grabObject);
 
   //gets jokes from db
   useEffect(() => { 
@@ -33,30 +38,57 @@ function UserJokes() {
     return () => unSubscribe();
   }, []);
 
- 
-let test;
-if (userJokesList) {
+  // gets jokes the current user has liked
+  useEffect(() => {
+    const auth = getAuth();
+    const listener = onAuthStateChanged(auth, async (user) => {
+      const jokeKarma = [];
+        async function getEmail() {
+          const collectionRef = collection(db, "users", user.email, "karma");
+          const unSubscribe = onSnapshot(collectionRef, (querySnapshot) => {
+          querySnapshot.forEach((doc) => {
+            jokeKarma.push({
+              id: doc.data().id,
+              jokeId: doc.data().jokeId,
+              karma: doc.data().karma,
+              votedUser: doc.data().votedUser,
+            })
+          });
+        })
+          return () => unSubscribe();
+        }
+        getEmail();
+        setKarmaList(jokeKarma);
+        setIsLoaded(true);
+    });
+    return () => {
+      listener();
+    };
+  }, [auth.currentUser]);
 
-  test = <>
-          {Object.values(userJokesList).map((entry) => (
-          <SubmittedJokes
-            joke = {entry.joke}
-            userName = {entry.userName}
-            user = {entry.user}
-            voteTally = {entry.voteTally}
-            id = {entry.id}
-          />
-        ))}
-        </>
-}
+  let test;       
+  if (userJokesList && karmaList) {
+    test = <>
+            {Object.values(userJokesList).map((entry) => (
+            <SubmittedJokes
+              joke = {entry.joke}
+              userName = {entry.userName}
+              user = {entry.user}
+              voteTally = {entry.voteTally}
+              id = {entry.id}
+              karmaList = {karmaList}
+            />
+          ))}
+          </>
+        }
 
   return (
     <React.Fragment>
     <div className=" ml-5 bg-transparent">
       <table className="scrollable">
         <thead className='border-bottom border-white bodyText fs-4 text-center'>Recent Jokes</thead>
-        <tbody>
-          {jokesLoaded ? test : null}
+        <tbody className='ml-5 bg-transparent'>
+          { (isLoaded && jokesLoaded) ? test : null}
         </tbody>
       </table>
     </div>
